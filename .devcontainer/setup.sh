2b931f37a4e08f3d3b2de5987346624ec1db5310
amyheather <a.heather2@exeter.ac.uk>
2026-05-19 12:25:31 +0100

    fix(codespace): trying different approach

--- .devcontainer/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Add any postCreate steps here (e.g. renv::restore())
-# RStudio Server is started in postAttachCommand, not here
-sudo bash -c 'echo "USER=rstudio" >> /etc/environment'
